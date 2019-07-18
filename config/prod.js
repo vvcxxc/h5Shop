f78daf550cc7992020ea7061671acc5d3a7b7259
songyizhe <769724661@qq.com>
2019-07-18 12:01:27 +0800

    登录测试

--- config/prod.js
+++ config/prod.js
@@ -2,8 +2,8 @@ module.exports = {
   env: {
     NODE_ENV: '"production"',
     BASIC_API: '"https://api.tdianyi.com/"',
-    OPEN_ID: '"test_open_id"',
-    TOKEN: '"test_token_auth"',
+    OPEN_ID: '"open_id"',
+    TOKEN: '"token_auth"',
     UNION_ID: '"unionid"'
   },
   defineConstants: {
