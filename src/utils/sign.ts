8748c92feb893309ffc44620e39ca114f62fec18
songyizhe <769724661@qq.com>
2019-07-18 10:56:13 +0800

    登录

--- src/utils/sign.ts
+++ src/utils/sign.ts
@@ -12,11 +12,11 @@ export const Login = () => {
     let url =  BASIC_API + 'wechat/wxoauth?code_id=0&from='+from;
     url = encodeURIComponent(url);
     let urls = 'http://wxauth.tdianyi.com/index.html?appid=wxecdd282fde9a9dfd&redirect_uri='+url+'&response_type=code&scope=snsapi_userinfo&connect_redirect=1&state=STATE&state=STATE';
-    // return window.location.href = urls;
+    return window.location.href = urls;
   }else{
     let url = BASIC_API +"ali/getZfbUserInfo";
     url = encodeURIComponent(url);
-    // window.location.href = BASIC_API +'/ali/zfbUserAuth?from='+from+'&code_id=0&url='+url;
+    window.location.href = BASIC_API +'/ali/zfbUserAuth?from='+from+'&code_id=0&url='+url;
   }
 
 }
