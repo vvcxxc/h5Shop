dd7b8ffea9fa06251148193e4d6e3a0c0ff51f8a
songyizhe <769724661@qq.com>
2019-07-18 13:32:23 +0800

    denglu

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: {
     NODE_ENV: '"production"',
-    BASIC_API: '"https://api.tdianyi.com/"',
-    OPEN_ID: '"open_id"',
-    TOKEN: '"token_auth"',
+    BASIC_API: '"https://test.api.tdianyi.com/"',
+    OPEN_ID: '"test_open_id"',
+    TOKEN: '"test_token_auth"',
     UNION_ID: '"unionid"'
   },
   defineConstants: {
