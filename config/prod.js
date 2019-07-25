1761485fa4b2c9b450f85f8feea6c51c067a0605
songyizhe <769724661@qq.com>
2019-07-25 15:51:26 +0800

    构建命令修改

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,11 @@
 module.exports = {
   env: {
     NODE_ENV: '"production"',
-    BASIC_API: '"https://test.api.tdianyi.com/"',
-    OPEN_ID: '"test_open_id"',
-    TOKEN: '"test_token_auth"',
+    BASIC_API: '"https://api.tdianyi.com/"',
+    OPEN_ID: '"open_id"',
+    TOKEN: '"token_auth"',
     UNION_ID: '"unionid"',
-    ALIPAY_USER_ID:'"test_alipay_user_id"'
+    ALIPAY_USER_ID:'"alipay_user_id"'
   },
   defineConstants: {
   },
