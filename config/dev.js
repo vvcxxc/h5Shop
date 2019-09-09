6bb244facbb1ad2148706cc039483ece2b535d11
songyizhe <769724661@qq.com>
2019-09-09 09:35:38 +0800

    修改接口

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     NODE_ENV: '"development"',
-    BASIC_API: '"https://test.api.tdianyi.com/"',
+    BASIC_API: '"http://test.api.tdianyi.com/"',
     // BASIC_API: '"https://release.api.tdianyi.com/"',
     OPEN_ID: '"test_open_id"',
     TOKEN: '"test_token_auth"',
