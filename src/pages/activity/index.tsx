88b0b53137a22da834c3ddca995f7e8e9c96b10a
songyizhe <769724661@qq.com>
2019-09-10 11:56:31 +0800

    跳转

--- src/pages/activity/index.tsx
+++ src/pages/activity/index.tsx
@@ -14,7 +14,7 @@ import "./activity.styl"
 import Coupon from "@/components/coupon/coupon"
 import { ACTION_JUMP } from "@/utils/constants"
 // import { getLocation } from "@/utils/getInfo"
-import {getLocation} from '@/utils/getInfo'
+import { getLocation } from '@/utils/getInfo'
 
 // import { connect } from "@tarojs/redux"
 
@@ -60,12 +60,19 @@ export default class Activity extends Component<ActivityProps> {
    * 用户动作
    */
   handleAction(action: string, data: any) {
-    switch(action) {
+    switch (action) {
       case ACTION_JUMP:
         const { type, id, gift_id, activity_id } = data
-        Taro.navigateTo({
-          url: `/pages/activity/pages/detail/detail?id=${id}&type=${type}&activity_id=${activity_id}&gift_id=${gift_id}`
-        })
+        if (type == 1) {
+          Taro.navigateTo({
+            url: '/pages/activity/appreciation/index?id=' + id + '&type=1&gift_id=' + gift_id + '&activity_id=' + activity_id
+            // url: `/pages/activity/pages/detail/detail?id=${id}&type=${type}&activity_id=${activity_id}&gift_id=${gift_id}`
+          })
+        } else {
+          Taro.navigateTo({
+            url: `/pages/activity/pages/detail/detail?id=${id}&type=${type}&activity_id=${activity_id}&gift_id=${gift_id}`
+          })
+        }
         break
       default:
         console.log("no action~")
@@ -143,7 +150,7 @@ export default class Activity extends Component<ActivityProps> {
           <View className="area-activity-list">
             <View
               // className="weui-grids"
-              style={{ border: " 0 none", display: "flex", height: "200px",  flexWrap: "wrap" }}>
+              style={{ border: " 0 none", display: "flex", height: "200px", flexWrap: "wrap" }}>
               {
                 activitys.map((item, index) => {
                   return (
@@ -152,7 +159,7 @@ export default class Activity extends Component<ActivityProps> {
                       onClick={this.goTo.bind(this, item.path)}
                       // className="weui-grid"
                       // hoverClass="weui-grid_active"
-                      style={{ border: "none", flex: "25%", display: "flex",  flexDirection: "column", justifyContent: "center", alignItems: "center" }}
+                      style={{ border: "none", flex: "25%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}
                     >
                       <Image className="weui-grid__icon" src={item.src} />
                       <View className="weui-grid__label">{item.text}</View>
