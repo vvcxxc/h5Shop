89e1d10d5236d8c6dc6682526830b4acb48d5f72
vvcxxc <asd123>
2019-09-24 11:25:03 +0800

    已参加的拼团增值活动分享

--- config/prod.js
+++ config/prod.js
@@ -8,7 +8,12 @@ module.exports = {
     ALIPAY_USER_ID:'"alipay_user_id"',
     LOGIN_URL: '"https://api.tdianyi.com"',
     GROUP_URL: '"http://mall.tdianyi.com/#/pages/activity/pages/group/group?id="',
-    APPRE_URL: '"http://mall.tdianyi.com/#/pages/activity/pages/appreciation/appreciation?id="'
+    APPRE_URL: '"http://mall.tdianyi.com/#/pages/activity/pages/appreciation/appreciation?id="',
+    APPRE_Details_URL: '"http://mall.tdianyi.com/#/pages/activity/appreciation/index?"',
+    GROUP_Details_URL: '"http://mall.tdianyi.com/#/pages/activity/group/index?"',
+    BUSINESS_URL:'"http://mall.tdianyi.com/#/pages/business/index?id="',
+    TICKETBUY_URL:'"http://mall.tdianyi.com/#/business-pages/ticket-buy/index?id="',
+    SETMEAL_URL:'"http://mall.tdianyi.com/#/business-pages/set-meal/index?id="'
   },
   defineConstants: {
   },
