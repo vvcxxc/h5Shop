588d77632097b273e8c713a7a9d7da995353a02f
vvcxxc <asd123>
2019-09-25 10:20:34 +0800

    测试

--- src/pages/activity/appreciation/index.tsx
+++ src/pages/activity/appreciation/index.tsx
@@ -185,7 +185,7 @@ export default class Appre extends Component<Props>{
         let { data } = res;
         console.log("request:linkMsg",linkMsg);
         wx.config({
-          debug: true,
+          debug: false,
           appId: data.appId,
           timestamp: data.timestamp,
           nonceStr: data.nonceStr,
