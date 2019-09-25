743f626af9470d1db902ffe708c11c920167744a
xiqoqilin <w3c_2014@163.com>
2019-09-25 09:46:42 +0800

    token

--- src/utils/sign.ts
+++ src/utils/sign.ts
@@ -16,7 +16,7 @@ export const Login = () => {
       return
     }
 
-    Cookie.set('test_token_auth', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdGVzdC5hcGkudGRpYW55aS5jb20vd2VjaGF0L3d4b2F1dGgiLCJpYXQiOjE1NjkyOTI2MzksImV4cCI6MTU2OTU5MjYzOSwibmJmIjoxNTY5MjkyNjM5LCJqdGkiOiJoWDlid2hmOUFKNmE3WVFUIiwic3ViIjo1MzQ1LCJwcnYiOiJmNmI3MTU0OWRiOGMyYzQyYjc1ODI3YWE0NGYwMmI3ZWU1MjlkMjRkIn0.jMcspZQGKrNuxZFYrh5uZHkWzJciG6YY_4fyH1DHnEk')
+    Cookie.set('test_token_auth', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vcmVsZWFzZS5hcGkudGRpYW55aS5jb20vd2VjaGF0L3d4b2F1dGgiLCJpYXQiOjE1NjkzNzQ4NTksImV4cCI6MTU2OTY3NDg1OSwibmJmIjoxNTY5Mzc0ODU5LCJqdGkiOiJ3M1FTTkZETjJYWkZ5NmhJIiwic3ViIjo2ODg5LCJwcnYiOiJmNmI3MTU0OWRiOGMyYzQyYjc1ODI3YWE0NGYwMmI3ZWU1MjlkMjRkIn0.d63s-k10hw9c8_NLaTzcqOkfuRCmfs5-HXNSTQ-CAvQ')
     // request({
 		// 	url: 'api/wap/testLogin'
 		// }).then((res: any) => {
