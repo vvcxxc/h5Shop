0518176afe45e87b021cc3862b816274eebd5951
xiqoqilin <w3c_2014@163.com>
2019-10-11 14:22:33 +0800

    解决活动列表页没有获取定位

--- src/utils/sign.ts
+++ src/utils/sign.ts
@@ -16,7 +16,7 @@ export const Login = () => {
       return
     }
 
-    Cookie.set('test_token_auth', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdGVzdC5hcGkudGRpYW55aS5jb20vd2VjaGF0L3d4b2F1dGgiLCJpYXQiOjE1Njk2NDEzOTYsImV4cCI6MTU2OTk0MTM5NiwibmJmIjoxNTY5NjQxMzk2LCJqdGkiOiJ5RlZUN2JhcWhKQjBkaXJSIiwic3ViIjo2ODQwLCJwcnYiOiJmNmI3MTU0OWRiOGMyYzQyYjc1ODI3YWE0NGYwMmI3ZWU1MjlkMjRkIn0.9-SkN5aYvXt1-XOCMV9mWb9Z-MQFruZrrzB_OZIz5go')
+    Cookie.set('test_token_auth', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdGVzdC5hcGkudGRpYW55aS5jb20vd2VjaGF0L3d4b2F1dGgiLCJpYXQiOjE1NzA3NzM3OTQsImV4cCI6MTU3MTA3Mzc5NCwibmJmIjoxNTcwNzczNzk0LCJqdGkiOiJ4Rkw5MEdNTmpSeXdrQUhNIiwic3ViIjo2ODg5LCJwcnYiOiJmNmI3MTU0OWRiOGMyYzQyYjc1ODI3YWE0NGYwMmI3ZWU1MjlkMjRkIn0.rXjqJwDlKfRn1vOH5TSYC7Qn582iCddMUoRMcY92XFM')
     // request({
 		// 	url: 'api/wap/testLogin'
 		// }).then((res: any) => {
