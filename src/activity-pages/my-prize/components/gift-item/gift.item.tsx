8469413805758665249b06c51a2347488858b28f
vvcxxc <asd123>
2019-11-01 15:41:22 +0800

    状态

--- src/activity-pages/my-prize/components/gift-item/gift.item.tsx
+++ src/activity-pages/my-prize/components/gift-item/gift.item.tsx
@@ -11,29 +11,40 @@ export default class GiftItem extends Component<{ data: any; onAction: any }> {
   /**
    * 点击事件
    */
-  handleClick =(e)=> {
+  handleClick = (e) => {
     const { action } = e.currentTarget.dataset
     const { data, onAction } = this.props
     onAction(action, data)
   }
   render() {
-    console.log('111',this.props);
+    console.log('111', this.props);
     const { data } = this.props
     return (
       <Block>
         <View className="gift-item">
+          {
+            data.status == 1 ? <View className="description_status"> 未使用</View> : (
+              data.status == 2 ? <View className="description_status"> 已使用</View> : (
+                data.status == 3 ? <View className="description_status"> 已过期</View> : null
+              )
+            )
+          }
+
           <View className="avatar">
             <Image className="icon" src={data.avatar} />
           </View>
           <View className="description">
+
             <Text className="item name">{data.prize_name}</Text>
             <Text className="item brief">{data.user_name}</Text>
             <Text className="item remark">{data.exceed_at}</Text>
           </View>
           <View className="actions" >
-            <View className="item" >
-             <Button className="item view" data-action="view" onClick={this.handleClick} style={{background:'linear-gradient(90deg,#FE7450,#FF2614)',color:"#fff"}} >查看二维码</Button>
-            </View>
+            {
+              data.status == 1 ? <View className="item" >
+                <Button className="item view" data-action="view" onClick={this.handleClick} style={{ background: 'linear-gradient(90deg,#FE7450,#FF2614)', color: "#fff" }} >查看二维码</Button>
+              </View> : null
+            }
           </View>
         </View>
       </Block>
