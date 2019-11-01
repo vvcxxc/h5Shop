71fdafdc0208a3b13809bc6c2ee167c50ec10349
vvcxxc <asd123>
2019-11-01 14:17:50 +0800

    解决冲突

--- config/test.js
+++ config/test.js
@@ -3,18 +3,6 @@ module.exports = {
     NODE_ENV: '"test"',
     // BASIC_API: '"https://api.tdianyi.com/"',
     UNION_ID: '"unionid"',
-<<<<<<< HEAD
-    // BASIC_API: '"https://test.api.tdianyi.com/"',
-    // OPEN_ID: '"test_open_id"',
-    // TOKEN: '"test_token_auth"',
-    // ALIPAY_USER_ID:'"test_alipay_user_id"',
-    // LOGIN_URL: '"https://test.api.tdianyi.com"',
-    BASIC_API: '"https://release.api.tdianyi.com/"',
-    LOGIN_URL: '"https://release.api.tdianyi.com"',
-    OPEN_ID: '"open_id"',
-    TOKEN: '"token_auth"',
-    ALIPAY_USER_ID:'"alipay_user_id"',
-=======
     BASIC_API: '"https://test.api.tdianyi.com/"',
     OPEN_ID: '"test_open_id"',
     TOKEN: '"test_token_auth"',
@@ -25,20 +13,13 @@ module.exports = {
     // OPEN_ID: '"open_id"',
     // TOKEN: '"token_auth"',
     // ALIPAY_USER_ID: '"alipay_user_id"',
->>>>>>> dev
     GROUP_URL: '"http://mall.tdianyi.com/#/pages/activity/pages/group/group?id="',
     APPRE_URL: '"http://mall.tdianyi.com/#/pages/activity/pages/appreciation/appreciation?id="',
     APPRE_Details_URL: '"http://mall.tdianyi.com/#/pages/activity/appreciation/index?"',
     GROUP_Details_URL: '"http://mall.tdianyi.com/#/pages/activity/group/index?"',
-<<<<<<< HEAD
-    BUSINESS_URL:'"http://mall.tdianyi.com/#/pages/business/index?id="',
-    TICKETBUY_URL:'"http://mall.tdianyi.com/#/business-pages/ticket-buy/index?id="',
-    SETMEAL_URL:'"http://mall.tdianyi.com/#/business-pages/set-meal/index?id="'
-=======
     BUSINESS_URL: '"http://mall.tdianyi.com/#/pages/business/index?id="',
     TICKETBUY_URL: '"http://mall.tdianyi.com/#/business-pages/ticket-buy/index?id="',
     SETMEAL_URL: '"http://mall.tdianyi.com/#/business-pages/set-meal/index?id="'
->>>>>>> dev
   },
   defineConstants: {
   },
