d3b5e0929ba51aea42873295fb002dbe1b91d6df
vvcxxc <asd123>
2019-11-07 10:44:29 +0800

    测试

--- src/activity-pages/components/cityPicker/index2.js
+++ src/activity-pages/components/cityPicker/index2.js
@@ -73,7 +73,7 @@ class PagePicker extends Component {
     onColumnChange = e => {
         console.log('round4',e);
         e.addEventListener(
-            'touchstart',
+            'touchmove',
             () => {
 
 
