475860382cfb18cc80cadc9430f8e577faaee8c1
vvcxxc <asd123>
2019-12-11 11:12:24 +0800

    测试

--- src/activity-pages/confirm-address/index.tsx
+++ src/activity-pages/confirm-address/index.tsx
@@ -50,8 +50,13 @@ export default class confirmAddress extends Component {
     };
 
     componentDidShow() {
+        let u = navigator.userAgent
+        if (u.indexOf('iPhone') > -1) {
+            console.log('iNoBounce', iNoBounce)
+            iNoBounce.enable()
+        }
         console.log(this.$router.params);
-        Taro.pageScrollTo({scrollTop: 0})
+        Taro.pageScrollTo({ scrollTop: 0 })
         let data;
         if (this.$router.params.address_id) {
             data = { youhui_id: this.$router.params.id, address_id: this.$router.params.address_id }
