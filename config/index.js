8150410d803f7dbd2a5af610facecb158d10e0ee
songyizhe <769724661@qq.com>
2020-02-14 17:37:51 +0800

    ios10test3

--- config/index.js
+++ config/index.js
@@ -105,13 +105,13 @@ const config = {
           }
         },
         // css modules 功能开关与相关配置
-        // cssModules: {
-        //   enable: true, // 默认为 false，如需使用 css modules 功能，则设为 true
-        //   config: {
-        //     namingPattern: 'module', // 转换模式，取值为 global/module，下文详细说明
-        //     generateScopedName: '[name]__[local]___[hash:base64:5]'
-        //   }
-        // }
+        cssModules: {
+          enable: true, // 默认为 false，如需使用 css modules 功能，则设为 true
+          config: {
+            namingPattern: 'module', // 转换模式，取值为 global/module，下文详细说明
+            generateScopedName: '[name]__[local]___[hash:base64:5]'
+          }
+        }
       },
       rules: [{
         test: /\.scss$/,
