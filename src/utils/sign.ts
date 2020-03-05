e8a8cb9f0d71d0b91cb7cd1a170b1421284127d0
songyizhe <769724661@qq.com>
2020-03-05 18:09:33 +0800

    test

--- src/utils/sign.ts
+++ src/utils/sign.ts
@@ -20,7 +20,8 @@ export const Login = () => {
   }else{
     if (type == 'wechat'){
       console.log(from,'from')
-      let url =  BASIC_API + 'wechat/wxoauth?code_id=0&from='+from;
+      // let url =  BASIC_API + 'wechat/wxoauth?code_id=0&from='+from;
+      let url = 'http://test.usercenter.tdianyi.com/v1/user/auth/auth_h5?code_id=0&from='+from
       if(process.env.NODE_ENV == 'test'){
         // url = LOGIN_URL+'/wechat/wxoauth?code_id=0&from='+from
         url = 'http://test.usercenter.tdianyi.com/v1/user/auth/auth_h5?code_id=0&from='+from
