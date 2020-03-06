7a3fdbab71cae152748d02aef372cff255c6a6f1
songyizhe <769724661@qq.com>
2020-03-06 14:13:33 +0800

    更换微信跳转地址

--- src/utils/sign.ts
+++ src/utils/sign.ts
@@ -20,15 +20,15 @@ export const Login = () => {
   }else{
     if (type == 'wechat'){
       console.log(from,'from')
-      let url =  BASIC_API + 'wechat/wxoauth?code_id=0&from='+from;
-      // let url = 'http://test.usercenter.tdianyi.com/v1/user/auth/auth_h5?code_id=0&from='+from
-      if(process.env.NODE_ENV == 'test'){
-        url = LOGIN_URL+'/wechat/wxoauth?code_id=0&from='+from
-        // url = 'http://test.usercenter.tdianyi.com/v1/user/auth/auth_h5?code_id=0&from='+from
-      }
+      // let url =  BASIC_API + 'wechat/wxoauth?code_id=0&from='+from;
+      let url = 'http://test.usercenter.tdianyi.com/v1/user/auth/auth_h5?code_id=0&from='+from
+      // if(process.env.NODE_ENV == 'test'){
+      //   url = LOGIN_URL+'/wechat/wxoauth?code_id=0&from='+from
+      //   // url = 'http://test.usercenter.tdianyi.com/v1/user/auth/auth_h5?code_id=0&from='+from
+      // }
       url = encodeURIComponent(url);
-      // let urls = 'http://test.wxauth.tdianyi.com/index_xcx.html?appid=wxfe816c3a5440ce7a&redirect_uri='+url+'&response_type=code&scope=snsapi_base&connect_redirect=1&state=STATE&state=STATE';
-      let urls = 'http://wxauth.tdianyi.com/index.html?appid=wxecdd282fde9a9dfd&redirect_uri='+url+'&response_type=code&scope=snsapi_base&connect_redirect=1&state=STATE&state=STATE';
+      let urls = 'http://test.wxauth.tdianyi.com/index_xcx.html?appid=wxfe816c3a5440ce7a&redirect_uri='+url+'&response_type=code&scope=snsapi_base&connect_redirect=1&state=STATE&state=STATE';
+      // let urls = 'http://wxauth.tdianyi.com/index.html?appid=wxecdd282fde9a9dfd&redirect_uri='+url+'&response_type=code&scope=snsapi_base&connect_redirect=1&state=STATE&state=STATE';
       return window.location.href = urls;
     }else{
       // let url = BASIC_API +"ali/getZfbUserInfo"; // 后台接口
