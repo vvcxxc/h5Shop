d9fea2758bbc22f5e95b84c5ac0b922a1256cc10
vvcxxc <asd123>
2020-03-07 17:38:54 +0800

    测试

--- src/activity-pages/personal/phoneInformation/index.tsx
+++ src/activity-pages/personal/phoneInformation/index.tsx
@@ -36,7 +36,7 @@ export default class PhoneInformation extends Component {
                 let { status_code, data, message } = res;
                 if (status_code == 200) {
                     this.setState({
-                        // phone: data.phone
+                        phone: data.phone
                     })
                 } else {
                     this.setState({ tipsShow: true, tipsInfo: message })
@@ -172,7 +172,7 @@ export default class PhoneInformation extends Component {
                             <View className='msgBox'> 您当前绑定的手机号码:{this.state.phone}</View>
                             <View className='infoBox'> 为了您的账户安全，请输入验证码</View>
                             <View className='inputBox'>
-                                <Input className='phoneInformationInput' type="text" onInput={this.handleCode.bind(this, '_code')} />
+                                <Input className='phoneInformationInput' type="text" onInput={this.handleCode.bind(this, '_code')} placeholder='请输入验证码'/>
                                 {
                                     this.state.is_ok ? (
                                         <View className='phoneInformationBtn' onClick={this.getCode.bind(this, 1)}> 获取验证码</View>
