08acbf5cce27bbb5de5a00951e060066978bd491
songyizhe <769724661@qq.com>
2020-03-11 18:38:28 +0800

    fix: 环境变量修复

--- config/test.js
+++ config/test.js
@@ -11,9 +11,9 @@ module.exports = {
     // ALIPAY_USER_ID: '"test_alipay_user_id"',
     // LOGIN_URL: '"http://test.api.tdianyi.com"',
     // AUTH_LOGIN_URL: '"http://test.wxauth.tdianyi.com/"',
-    // USER_API: '"http://test.usercenter.tdianyi.com/"'，
-    // WX_APPID: 'wxfe816c3a5440ce7a',
-    // ALI_APPID: '2018080960968490',
+    // USER_API: '"http://test.usercenter.tdianyi.com/"',
+    // WX_APPID: "'wxfe816c3a5440ce7a'",
+    // ALI_APPID: "'2018080960968490'",
 
     // 预发布环境
     BASIC_API: '"http://release.api.tdianyi.com/"',
@@ -23,8 +23,8 @@ module.exports = {
     ALIPAY_USER_ID: '"alipay_user_id"',
     AUTH_LOGIN_URL: '"https://wxauth.tdianyi.com/"',
     USER_API: '"https://release.usercenter.tdianyi.com/"',
-    WX_APPID: 'wxecdd282fde9a9dfd',
-    ALI_APPID: '2017111509949310',
+    WX_APPID: "'wxecdd282fde9a9dfd'",
+    ALI_APPID: "'2017111509949310'",
 
     GROUP_URL: '"http://test.mall.tdianyi.com/pages/activity/pages/group/group?id="',
     APPRE_URL: '"http://test.mall.tdianyi.com/pages/activity/pages/appreciation/appreciation?id="',
