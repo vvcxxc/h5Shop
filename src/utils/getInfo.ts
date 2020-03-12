d76427593b756d6a004b8b43738c61da19ebb29a
songyizhe <769724661@qq.com>
2020-03-12 14:25:24 +0800

    fix：一键授权

--- src/utils/getInfo.ts
+++ src/utils/getInfo.ts
@@ -162,7 +162,8 @@ export const getLocation = () => {
 
 const AUTH_LOGIN_URL = process.env.AUTH_LOGIN_URL
 const USER_API = process.env.USER_API
-
+const WX_APPID = process.env.WX_APPID
+const ALI_APPID = process.env.ALI_APPID
 export const getUserInfo = () => {
   let from = window.location.href
   if(from.indexOf('&from') > -1){
@@ -171,15 +172,28 @@ export const getUserInfo = () => {
   }
   let type = getBrowserType();
   if (type == 'wechat'){
-    let url = USER_API + 'v1/user/auth/get_wx_user_info?code_id=0&from='+from
+    encodeURIComponent(from);
+    let url = USER_API + 'v1/user/auth/auth_h5?code_id=0&from='+from
+    // if(process.env.NODE_ENV == 'test'){
+    //   url = LOGIN_URL+'/wechat/wxoauth?code_id=0&from='+from
+    //   // url = 'http://test.usercenter.tdianyi.com/v1/user/auth/auth_h5?code_id=0&from='+from
+    // }
     url = encodeURIComponent(url);
-    let urls = AUTH_LOGIN_URL + 'index_xcx.html?appid=wxfe816c3a5440ce7a&redirect_uri='+url+'&response_type=code&scope=snsapi_userinfo&connect_redirect=1&state=STATE&state=STATE';
+    let urls = AUTH_LOGIN_URL + 'index_xcx.html?appid='+ WX_APPID +'&redirect_uri='+url+'&response_type=code&scope=snsapi_userinfo&connect_redirect=1&state=STATE&state=STATE';
+    // let urls = 'http://wxauth.tdianyi.com/index.html?appid=wxecdd282fde9a9dfd&redirect_uri='+url+'&response_type=code&scope=snsapi_base&connect_redirect=1&state=STATE&state=STATE';
     return window.location.href = urls;
   }else{
-    from = encodeURIComponent(from); // 当前页面
-    let url = USER_API + 'v1/user/auth/get_ali_user_info?code_id=227&from='+ from
-    url = encodeURIComponent(url);
-    let urls = AUTH_LOGIN_URL + 'ali.html?appid=2018080960968490&redirect_uri='+url+'&scope=auth_user&state=STATE'
-    return window.location.href = urls;
+      // let url = BASIC_API +"ali/getZfbUserInfo"; // 后台接口
+      from = encodeURIComponent(from); // 当前页面
+      // url = encodeURIComponent(url);
+
+      // window.location.href = BASIC_API +'ali/zfbUserAuth?from='+from+'&code_id=227&url='+url;
+
+      // 新版授权
+      // let url = process.env.ALIPAY_LOGIN_URL + 'v1/user/auth/auth_ali?code_id=227&from='+ from
+      let url = USER_API + 'v1/user/auth/auth_ali?code_id=227&from='+ from
+      url = encodeURIComponent(url);
+      let urls = AUTH_LOGIN_URL + 'ali.html?appid='+ ALI_APPID +'&redirect_uri='+url+'&scope=auth_user&state=STATE'
+      return window.location.href = urls;
   }
 }
