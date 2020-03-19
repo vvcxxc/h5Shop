4f0ebc106510e2c8a13e1ba17d70de3d366a55b0
vvcxxc <asd123>
2020-03-19 16:10:42 +0800

    fix:活动购买底部定位兼容性尝试

--- src/utils/sign.ts
+++ src/utils/sign.ts
@@ -19,7 +19,7 @@ export const Login = () => {
   }
   let type = getBrowserType();
   if (process.env.NODE_ENV == 'development') {
-    Cookie.set('test_token_auth', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdGVzdC51c2VyY2VudGVyLnRkaWFueWkuY29tL3YxL3VzZXIvYXV0aC9yZWxvZ2luIiwiaWF0IjoxNTg0NTAwMDAxLCJleHAiOjE1ODQ1OTAwMDEsIm5iZiI6MTU4NDUwMDAwMSwianRpIjoiUTNJUFAyVkZTRVFKQWJvVSIsInN1YiI6NzcwMiwicHJ2IjoiNTg3ZWQ0ZWI0ZmY2YjBiMmQ4OTZhOWI3YjcxMDRlNzBhNWI3YTAwMCJ9.y7Mhi6DgZimR7N1RL1rzAAdXAPRB8hxGOTdoQi1FV1A')
+    Cookie.set('test_token_auth', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdGVzdC51c2VyY2VudGVyLnRkaWFueWkuY29tL3YxL3VzZXIvYXV0aC9hdXRoX2g1IiwiaWF0IjoxNTg0NTk5NDUzLCJleHAiOjE1ODQ2ODk0NTMsIm5iZiI6MTU4NDU5OTQ1MywianRpIjoicm10ZzQ4Z1pUcnVpVmZPWiIsInN1YiI6MzY3NTQsInBydiI6IjU4N2VkNGViNGZmNmIwYjJkODk2YTliN2I3MTA0ZTcwYTViN2EwMDAifQ.iikRhNhzUaja8wrzc6mGxb4ewNPl0WaX-PnzMuDUuD0')
   } else {
     if (type == 'wechat') {
       console.log(from, 'from')
