ca7f678d6eabd49d74e99d0ef5e4a456cc9cbb94
vvcxxc <asd123>
2020-03-23 15:18:48 +0800

    styl:样式

--- src/pages/activity/pages/appreciation/appreciation.tsx
+++ src/pages/activity/pages/appreciation/appreciation.tsx
@@ -541,7 +541,7 @@ export default class Appreciation extends Component {
         {/* 去首页 */}
         {
           this.state.isFromShare ? (
-            <View style={{position: 'fixed', bottom: '20px', right: '20px', zIndex: 88, width: '80px', height: '80px' }} onClick={this.handleGoHome.bind(this)}>
+            <View style={{position: 'fixed', bottom: '80px', right: '20px', zIndex: 88, width: '80px', height: '80px' }} onClick={this.handleGoHome.bind(this)}>
               <Image src={require('../../../../assets/go-home/go_home.png')} style={{ width: '80px', height: '80px' }} />
             </View>
           ) : ''
