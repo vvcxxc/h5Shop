e0f194f143e19e043cff4e748267a83e4801f57f
xiqoqilin <w3c_2014@163.com>
2020-03-24 17:42:21 +0800

    改样式

--- src/business-pages/set-meal/index.tsx
+++ src/business-pages/set-meal/index.tsx
@@ -187,7 +187,7 @@ export default class AppreActivity extends Component {
         </View> */}
         <View className="appre-info-content">
           <View className="appre-info-title">
-            <View className="appre-info-title-label">兑换券</View>
+            <View className="appre-info-title-label">小熊敬礼</View>
             <View className="appre-info-title-text">{this.state.coupon.yname}</View>
           </View>
           <View className="appre-info-price">
@@ -280,7 +280,7 @@ export default class AppreActivity extends Component {
                       <View className="good_detail_info">
                         <View className="good_title">
                           <View className="good_type">
-                            <View className="text">{this.state.recommend[0].youhui_type == 0 ? "兑换券" : "现金券"}</View>
+                            <View className="text">{this.state.recommend[0].youhui_type == 0 ? "小熊敬礼" : "到店支付可用"}</View>
                           </View>
                           <View className="good_cash">{this.state.recommend[0].yname}</View>
                         </View>
@@ -309,7 +309,7 @@ export default class AppreActivity extends Component {
                       <View className="good_detail_info">
                         <View className="good_title">
                           <View className="good_type">
-                            <View className="text">{this.state.recommend[1].youhui_type == 0 ? "兑换券" : "现金券"}</View>
+                            <View className="text">{this.state.recommend[1].youhui_type == 0 ? "小熊敬礼" : "到店支付可用"}</View>
                           </View>
                           <View className="good_cash">{this.state.recommend[1].yname}</View>
                         </View>
@@ -339,7 +339,7 @@ export default class AppreActivity extends Component {
                         <View className="good_detail_info">
                           <View className="good_title">
                             <View className="good_type">
-                              <View className="text">{item.youhui_type == 0 ? "兑换券" : "现金券"}</View>
+                              <View className="text">{item.youhui_type == 0 ? "小熊敬礼" : "到店支付可用"}</View>
                             </View>
                             <View className="good_cash">{item.yname}</View>
                           </View>
