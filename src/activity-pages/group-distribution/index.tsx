e78cf365bf7b91540319d46047936d9127545f58
vvcxxc <asd123>
2020-03-27 17:40:09 +0800

    fix:新增地址id

--- src/activity-pages/group-distribution/index.tsx
+++ src/activity-pages/group-distribution/index.tsx
@@ -19,6 +19,7 @@ export default class distributionDetail extends Component {
         sumMoney: 0,
         data: {
             address: {
+                id: 0,
                 city: "",
                 detail: "",
                 district: "",
@@ -169,6 +170,7 @@ export default class distributionDetail extends Component {
             gift_id: this.state.chooseGift && this.state.data.youhui.gift_id ? this.state.data.youhui.gift_id : undefined,
             is_delivery: this.state.chooseDistribution && this.state.data.youhui.is_delivery ? 1 : 0,
             type: this.$router.params.activityType,
+            address_id: this.state.data.address.id,
             xcx: 0,
             number: 1,
         };
