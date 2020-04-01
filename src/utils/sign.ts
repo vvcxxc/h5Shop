52aa2fbfa12b27f06f826f2bd076a9263746264f
vvcxxc <asd123>
2020-04-01 17:29:43 +0800

    fix:状态

--- src/utils/sign.ts
+++ src/utils/sign.ts
@@ -20,7 +20,7 @@ export const Login = () => {
   }
   let type = getBrowserType();
   if(process.env.NODE_ENV == 'development'){
-    Cookie.set('test_token_auth', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdGVzdC51c2VyY2VudGVyLnRkaWFueWkuY29tL3YxL3VzZXIvYXV0aC9hdXRoX2g1IiwiaWF0IjoxNTg1NjE0ODQyLCJleHAiOjE1ODU3MDQ4NDIsIm5iZiI6MTU4NTYxNDg0MiwianRpIjoiRWV5U0lpZkJlZUViTnExZiIsInN1YiI6MzY3NTQsInBydiI6IjU4N2VkNGViNGZmNmIwYjJkODk2YTliN2I3MTA0ZTcwYTViN2EwMDAifQ.zPsK-v2bfnJqzDbYXdRKpNPLHT-sPNSk49j0a9z0pr8')
+    Cookie.set('test_token_auth', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3JlbGVhc2UudXNlcmNlbnRlci50ZGlhbnlpLmNvbS92MS91c2VyL2F1dGgvYXV0aF9oNSIsImlhdCI6MTU4NTczMjkyOCwiZXhwIjoxNTg1ODIyOTI4LCJuYmYiOjE1ODU3MzI5MjgsImp0aSI6IkQ5dFd1WDdSUkhHR1VWSlEiLCJzdWIiOjMwODk4LCJwcnYiOiI1ODdlZDRlYjRmZjZiMGIyZDg5NmE5YjdiNzEwNGU3MGE1YjdhMDAwIn0.QQQ8FuFXG9QVyj6-bOzIrEfWnEBu0pWS1Vm8X6tHS1Y')
 
   }else{
     if (type == 'wechat'){
