21649cb1842fdb80e4963f90c6802f7656c5eedf
vvcxxc <asd123>
2020-04-03 09:14:31 +0800

    文案

--- src/business-pages/confirm-order/index.tsx
+++ src/business-pages/confirm-order/index.tsx
@@ -32,7 +32,7 @@ export default class ConfirmOrder extends Component {
       id: "",
       sname: ""
     },
-
+    tipsMessage: ''
   };
   componentWillMount() {
 
@@ -115,63 +115,68 @@ export default class ConfirmOrder extends Component {
     })
       .then((res: any) => {
         Taro.hideLoading();
-        if (_type == 1) {
-          //微信
-          window.WeixinJSBridge.invoke(
-            'getBrandWCPayRequest', {
-            "appId": res.data.appId,
-            "timeStamp": res.data.timeStamp,
-            "nonceStr": res.data.nonceStr,
-            "package": res.data.package,
-            "signType": res.data.signType,
-            "paySign": res.data.paySign
-          },
-            function (res) {
-              if (res.err_msg == "get_brand_wcpay_request:ok") {
-                //微信成功
+        if (res.code == 200) {
+          if (_type == 1) {
+            //微信
+            window.WeixinJSBridge.invoke(
+              'getBrandWCPayRequest', {
+              "appId": res.data.appId,
+              "timeStamp": res.data.timeStamp,
+              "nonceStr": res.data.nonceStr,
+              "package": res.data.package,
+              "signType": res.data.signType,
+              "paySign": res.data.paySign
+            },
+              function (res) {
+                if (res.err_msg == "get_brand_wcpay_request:ok") {
+                  //微信成功
+                  Taro.showToast({ title: '支付成功', icon: 'none' })
+
+                  Taro.switchTab({
+                    url: '/pages/order/index',
+                    success: (e) => {
+                      let page = Taro.getCurrentPages().pop();
+                      if (page == undefined || page == null) return;
+                      page.onShow();
+                    }
+                  })
+                } else {
+                  Taro.showToast({ title: '支付失败', icon: 'none' })
+                }
+              }
+            );
+          }
+          else if (_type == 2) {
+            //支付宝
+            window.AlipayJSBridge.call('tradePay', {
+              tradeNO: res.data.alipayOrderSn, // 必传，此使用方式下该字段必传
+              // bizType:"xxx",                          // 非必传，默认为 “trade”
+              // bizSubType:"",                          // 非必传，默认为 “”
+              // bizContext:""                           // 非必传，默认为H5启动选项(safePayContext)
+            }, res => {
+              if (res.resultCode === "9000") {
                 Taro.showToast({ title: '支付成功', icon: 'none' })
 
+                //支付宝成功
                 Taro.switchTab({
                   url: '/pages/order/index',
                   success: (e) => {
                     let page = Taro.getCurrentPages().pop();
                     if (page == undefined || page == null) return;
                     page.onShow();
+
                   }
                 })
               } else {
                 Taro.showToast({ title: '支付失败', icon: 'none' })
               }
-            }
-          );
-        }
-        else if (_type == 2) {
-          //支付宝
-          window.AlipayJSBridge.call('tradePay', {
-            tradeNO: res.data.alipayOrderSn, // 必传，此使用方式下该字段必传
-            // bizType:"xxx",                          // 非必传，默认为 “trade”
-            // bizSubType:"",                          // 非必传，默认为 “”
-            // bizContext:""                           // 非必传，默认为H5启动选项(safePayContext)
-          }, res => {
-            if (res.resultCode === "9000") {
-              Taro.showToast({ title: '支付成功', icon: 'none' })
-
-              //支付宝成功
-              Taro.switchTab({
-                url: '/pages/order/index',
-                success: (e) => {
-                  let page = Taro.getCurrentPages().pop();
-                  if (page == undefined || page == null) return;
-                  page.onShow();
-
-                }
-              })
-            } else {
-              Taro.showToast({ title: '支付失败', icon: 'none' })
-            }
-          })
+            })
+          } else {
+            console.log(_type)
+          }
         } else {
-          console.log(_type)
+          this.setState({ tipsMessage: res.message })
+          // Taro.showToast({ title: res.message, icon: 'none' })
         }
       })
   }
@@ -195,8 +200,8 @@ export default class ConfirmOrder extends Component {
     return (
       <View className="confirm-order" >
         <View className="content">
-          <View className="flex center">
-            <View className="item label">{this.state.store.sname}{this.state.coupon.yname}</View>
+          <View className="flex center snameBox">
+            <View className="item label sname">{this.state.store.sname}{this.state.coupon.yname}</View>
             <View>{this.state.coupon.pay_money}元</View>
           </View>
           <View className="flex center">
@@ -229,18 +234,17 @@ export default class ConfirmOrder extends Component {
             ￥{this.state.tempNum} 去支付
           </View>
         </View>
+
+        {
+          this.state.tipsMessage ? <View className="tips-mask">
+            <View className="tips-content">
+              <View className="tips-title">购买失败</View>
+              <View className="tips-info">{this.state.tipsMessage}</View>
+              <View className="tips-btn" onClick={() => { this.setState({ tipsMessage: '' }) }}>确定</View>
+            </View>
+          </View> : null
+        }
       </View>
     );
   }
 }
-
-
-
-// Taro.switchTab({
-//   url: '/pages/order/index',
-//   success: () => {
-//     var page = Taro.getCurrentPages().pop();
-//     if (page == undefined || page == null) return;
-//     page.onLoad();
-//   }
-// })
