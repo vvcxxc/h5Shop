fdd14a4c3af3df67e241ef724fa8546f4e607176
songyizhe <769724661@qq.com>
2020-04-13 09:14:03 +0800

    a

--- src/pages/activity/group/index.tsx
+++ src/pages/activity/group/index.tsx
@@ -151,6 +151,7 @@ export default class GroupActivity extends Component {
           let isPostage = false;
           if (res.data.gift_id && res.data.gift.mail_mode == 2) { isPostage = true; }
           let new_time = new Date().getTime()//ql
+          console.log(new Date(res.data.activity_end_time).getTime(),'333')
           new Date(res.data.activity_end_time).getTime() + 86399000 < new_time ? this.setState({ allowGroup: '已结束' }) : null
           new Date(res.data.activity_begin_time).getTime() > new_time ? this.setState({ allowGroup: '暂未开始' }) : null
 
