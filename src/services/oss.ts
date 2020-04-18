31816cbf4eb2c35e562ed73629454e40ab3f1cb5
songyizhe <769724661@qq.com>
2020-04-18 09:20:06 +0800

    fix: oss参数

--- src/services/oss.ts
+++ src/services/oss.ts
@@ -96,6 +96,7 @@ export default async function upload(files: any) {
                 signature: oss_data.signature,
                 success_action_status: '200',
                 callback: oss_data.callback,
+                host: oss_data.host
                 // file: imgUrl
             },
             success: (res) => {
